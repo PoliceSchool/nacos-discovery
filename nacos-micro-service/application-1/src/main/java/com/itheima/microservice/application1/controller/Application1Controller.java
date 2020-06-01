c511e5049effab9c0eebf02e3988839e73e4ff6c
hechang <hechang@wakedata.com>
2020-06-01 10:56:16 +0800

    fourth commit

--- nacos-micro-service/application-1/src/main/java/com/itheima/microservice/application1/controller/Application1Controller.java
+++ nacos-micro-service/application-1/src/main/java/com/itheima/microservice/application1/controller/Application1Controller.java
@@ -1,6 +1,8 @@
 package com.itheima.microservice.application1.controller;
 
 import com.itheima.microservice.service1.api.ConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,14 @@ public class Application1Controller {
 
     @org.apache.dubbo.config.annotation.Reference
     ConsumerService consumerService;
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
 
     @GetMapping("/service")
     public String service() {
         String service = consumerService.service();
+        String name = applicationContext.getEnvironment().getProperty("common.name");
+        System.out.println(name);
         return service;
     }
 }
